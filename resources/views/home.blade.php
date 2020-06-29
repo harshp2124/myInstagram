d6a742c9a380acc6ab49fd5ed88165899bb715c2
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-06-29 14:42:24 +0530

    Made basic front-end for user profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.famd4-1.fna.fbcdn.net/v/t51.2885-19/s320x320/104214292_1124497261256342_5995623851880189325_n.jpg?_nc_ht=instagram.famd4-1.fna.fbcdn.net&_nc_ohc=epm3uqw6e0IAX85tAgm&oh=7e8be6eb5fc9f0a6739db54c56e50db8&oe=5F225CB2" class="rounded-circle w-100">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>harsh2124</h1></div>
+            <div class="d-flex">
+                <div><strong>153</strong> posts</div>
+                <div class="pl-5"><strong>100</strong> followers</div>
+                <div class="pl-5"><strong>312</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">Harsh Patel</div>
+            <div>A software Developer!</div>
+            <div><a href="http://harsh2124p.wordpress.com/">http://harsh2124p.wordpress.com/</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.famd4-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/106108528_718754082002855_6226808487402990950_n.jpg?_nc_ht=instagram.famd4-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=LDcPlGI74s0AX_6Jgj0&oh=72accc930bf59235b81c272b71722b6e&oe=5F218EAB" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://instagram.famd4-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c175.0.730.730a/s640x640/105377718_268839034547707_445246563180082541_n.jpg?_nc_ht=instagram.famd4-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=rmXSlHK9Xb8AX_2x4dY&oh=a423de701624d93b9c013998d2374483&oe=5F22840B" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://instagram.famd4-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.94.1080.1080a/s640x640/103638316_665064300740102_1162472666644730795_n.jpg?_nc_ht=instagram.famd4-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=7nzTjLMmsAQAX9G6jTa&oh=ddc105f49f12aab5120241f8969f4732&oe=5F21D64A" class="w-100">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand font-weight-bold" href="{{ url('/') }}" style="font-family: 'Satisfy', cursive; font-size: 25px;">
                     myInstagram
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
