6de64db2f5513aa6d346c2026db5cbad512d03af
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-07-01 19:13:20 +0530

    Adding default profile photo

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,11 +10,14 @@
             <div>
                 <div class="d-flex align-items-center">
                     <div class="pr-3">
-                        <img src="/storage/{{ $post->user->profile->image }}" class="w-100 rounded-circle" style="max-width: 40px;">
+                        <img src="{{ $post->user->profile->profileImage() }}" class="w-100 rounded-circle" style="max-width: 40px;">
                     </div>
                     <div>
                         <div class="font-weight-bold"><a href="/{{ $post->user->username }}"><span class="text-dark">{{ $post->user->username }}</span></a></div>
                     </div>
+                    <div class="pl-1">•</div>
+                    
+                    <a href="#" class="pl-1 font-weight-bold">Follow</a>
                 </div>
                 <hr>
 
